66b004df740f71dbc8200a5025b50e42c4de42f4
laoagv <laoagvik@gmail.com>
2023-06-12 03:45:53 +0500

    V11

--- src/api.js
+++ src/api.js
@@ -132,7 +132,7 @@ export function refreshToken(token){
 }
 
 export async function getUserData(){
-    const response = await fetchWithAuth("api/user", {method: 'GET', mode: 'cors',})
+    const response = await fetchWithAuth("api/user", {method: 'GET', mode: 'no-cors',})
     const data =  await response.json()       
     localStorage.setItem('userData', JSON.stringify(await data))
     return 
