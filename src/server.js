56e782fe468a7f664b0e2947eeabb480c048883e
laoagv <laoagvik@gmail.com>
2023-06-12 03:05:38 +0500

    V7

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ export function server(){
     return "https://dimaloh.pythonanywhere.com/"
 }
 export function front(){
-    return "https://edukid-front-25n5juj59-laoagv.vercel.app/"
+    return "https://edukid-front.vercel.app/"
 }
